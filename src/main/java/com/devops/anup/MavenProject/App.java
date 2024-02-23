17893c24335db950ee468de7ae643fe28c370f4c
Anup-Panigrahy <anup.kumarpanigrahy12@gmail.com>
2024-02-23 13:52:10 +0530

    First commit of the project whose name is MavenProject

---
+++ src/main/java/com/devops/anup/MavenProject/App.java
@@ -0,0 +1,13 @@
+package com.devops.anup.MavenProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
